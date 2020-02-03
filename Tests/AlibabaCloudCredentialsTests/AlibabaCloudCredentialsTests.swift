51f2bb1f73d5f759fd771bb4dee6a2d2511f38b7
axios <axiosleo@foxmail.com>
2020-02-03 17:39:49 +0800

    add tests

--- Tests/AlibabaCloudCredentialsTests/AlibabaCloudCredentialsTests.swift
+++ Tests/AlibabaCloudCredentialsTests/AlibabaCloudCredentialsTests.swift
@@ -6,9 +6,6 @@ import Alamofire
 @testable import AlibabaCloudCredentials
 
 final class AlibabaCloudCredentialsTests: XCTestCase {
-    override func setUp() {
-        super.setUp()
-    }
 
     public func testAKCredentials() {
         let config: Configuration = Configuration()
@@ -48,6 +45,59 @@ final class AlibabaCloudCredentialsTests: XCTestCase {
         XCTAssertEqual(credential.accessKeySecret, config.accessKeySecret)
         XCTAssertEqual(credential.securityToken, config.securityToken)
         XCTAssertEqual(credential.expiration, config.expiration)
+    }
+
+    public func testRamRoleArnCredential() {
+        let date: Date = Date().addingTimeInterval(10000.0)
+
+        let config: Configuration = Configuration()
+        config.accessKeyId = "accessKeyId"
+        config.accessKeySecret = "accessKeySecret"
+        config.securityToken = "security token"
+        config.expiration = date.timeIntervalSince1970
+        config.policy = "policy"
+        config.roleArn = "role arn"
+
+        let provider = CredentialsProvider(config: config)
+        let credential: RamRoleArnCredential = provider.getCredential(credentialType: CredentialType.RamRoleArn) as! RamRoleArnCredential
+
+        XCTAssertEqual(credential.accessKeyId, config.accessKeyId)
+        XCTAssertEqual(credential.accessKeySecret, config.accessKeySecret)
+        XCTAssertEqual(credential.securityToken, config.securityToken)
+        XCTAssertEqual(credential.expiration, config.expiration)
+        XCTAssertEqual(credential.policy, config.policy)
+        XCTAssertEqual(credential.roleArn, config.roleArn)
+    }
+
+    public func testRsaKeyPairCredential() {
+        let date: Date = Date().addingTimeInterval(10000.0)
+
+        let config: Configuration = Configuration()
+        config.publicKeyId = "publicKeyId"
+        config.privateKeySecret = "privateKeySecret"
+        config.expiration = date.timeIntervalSince1970
+
+        let provider = CredentialsProvider(config: config)
+        let credential: RsaKeyPairCredential = provider.getCredential(credentialType: CredentialType.RsaKeyPair) as! RsaKeyPairCredential
+
+        XCTAssertEqual(credential.publicKeyId, config.publicKeyId)
+        XCTAssertEqual(credential.privateKeySecret, config.privateKeySecret)
+        XCTAssertEqual(credential.expiration, config.expiration)
+
+    }
+
+    public func testStsCredential() {
+        let config: Configuration = Configuration()
+        config.accessKeyId = "accessKeyId"
+        config.accessKeySecret = "accessKeySecret"
+        config.securityToken = "security token"
+
+        let provider = CredentialsProvider(config: config)
+        let credential: StsCredential = provider.getCredential(credentialType: CredentialType.STS) as! StsCredential
+
+        XCTAssertEqual(credential.accessKeyId, config.accessKeyId)
+        XCTAssertEqual(credential.accessKeySecret, config.accessKeySecret)
+        XCTAssertEqual(credential.securityToken, config.securityToken)
     }
 
     static var allTests = [
